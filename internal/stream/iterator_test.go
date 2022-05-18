c690ee75d44e1b3bb412c75c77907fd6eeccf3b2
WK WK <witold.kepczynski@gmail.com>
2022-05-18 23:08:23 +0200

    unit tests

--- internal/stream/iterator_test.go
+++ internal/stream/iterator_test.go
@@ -2,13 +2,16 @@ package stream_test
 
 import (
 	"context"
+	"fmt"
+	"math/rand"
 	"pilsner/internal/communication"
 	"pilsner/internal/stream"
 	"testing"
 )
 
 type dataMock struct {
-	counter int
+	counter        int
+	switchPosition int
 }
 
 func (d *dataMock) GetIterator(terminate context.Context) stream.Iterator {
@@ -23,7 +26,7 @@ func (d *dataMock) Put(item communication.Item) {
 
 func (d *dataMock) TryGet(position int) (error, communication.Item) {
 
-	if d.counter == 1 {
+	if d.counter == d.switchPosition {
 		d.counter++
 		return nil, communication.Item{Id: stream.NoItemId}
 	}
@@ -32,34 +35,43 @@ func (d *dataMock) TryGet(position int) (error, communication.Item) {
 
 }
 
-// TODO fuzz it
 func TestIteratorShouldSwitchToNotifier(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		t.Run(fmt.Sprintf("Testing iteration %d", i), testIteratorShouldSwitchToNotifier)
+	}
 
-	notifier := make(chan int, 10)
+}
 
-	notifier <- 1
-	notifier <- 2
+func testIteratorShouldSwitchToNotifier(t *testing.T) {
+
+	count := rand.Intn(100)
+
+	t.Logf("Random is %d", count)
+
+	notifier := make(chan int, count)
+
+	for i := 0; i < count; i++ {
+		notifier <- i
+	}
 
 	ctx := context.Background()
 
-	dataSource := dataMock{}
+	dataSource := dataMock{
+		switchPosition: 50,
+	}
 
 	iterator := stream.NewIterator(&dataSource, notifier, ctx)
 
-	_, iteratorItem := iterator.Next()
+	for j := 0; j < count; j++ {
+		_, iteratorItem := iterator.Next()
 
-	_, notifierItem1 := iterator.Next()
+		if (j < dataSource.switchPosition) && (iteratorItem.Source != "Iterator") {
+			t.Errorf("Expected Iterator source recieved %s ", iteratorItem.Source)
+		}
+		if (j >= dataSource.switchPosition) && (iteratorItem.Source != "Notifier") {
+			t.Errorf("Expected Notifier source recieved %s ", iteratorItem.Source)
+		}
 
-	_, notifierItem2 := iterator.Next()
-
-	if iteratorItem.Source != "Iterator" {
-		t.Errorf("Expected Iterator source recieved %s ", iteratorItem.Source)
 	}
 
-	if notifierItem1.Source != "Notifier" {
-		t.Errorf("Expected Notifier source recieved %s ", notifierItem1.Source)
-	}
-	if notifierItem2.Source != "Notifier" {
-		t.Errorf("Expected Notifier source recieved %s ", notifierItem1.Source)
-	}
 }
